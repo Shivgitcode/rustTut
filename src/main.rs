a3556fa8970e1b348368d8c4c8e499dbecf3932b
shivansh <shivneeraj2004@gmail.com>
2024-05-23 21:24:32 +0530

    rust tut 1

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x = -5; //int
+    let y: u32 = 1000; //unsigned int
+    let z: f32 = 1000.001; //float
+    let is_male: bool = true;
+    println!("x:{}, y:{}, z:{}, isMale:{}", x, y, z, is_male);
+}
